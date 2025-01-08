880229019f31c9dc62bc2c1aae362e41c8117094
Ahmed <vactum0@gmail.com>
2025-01-08 15:09:02 +0000

    update

--- app_v2.py
+++ app_v2.py
@@ -933,6 +933,7 @@ elif st.session_state.step == 3:
     # Allow users to input table data
     st.write("### Add INVOICE Details")
     st.toast('This page is not for TIMESHEET!')
+    st.toast('This page is for INVOICE DETAILS ONLY!')
     for i, row in enumerate(st.session_state.table_data):
         cols = st.columns(4)
         # row["date"] = cols[0].date_input("Date / Period", datetime.strptime(row["date"], "%d-%m-%Y").date() if row["date"] else None, key=f"date_{i}", format='DD/MM/YYYY')
@@ -963,7 +964,7 @@ elif st.session_state.step == 3:
         st.session_state.step=4
         st.experimental_rerun()
     if st.button("Back"):
-        st.session_state.step=3
+        st.session_state.step=2
         st.experimental_rerun()
     
 
